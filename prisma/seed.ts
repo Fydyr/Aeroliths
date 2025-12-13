cb4ebca8a37a3e4b32839713cf1365b4f15e40c9
Enzo <fournierenzo62@gmail.com>
2025-12-13 16:47:59 +0100

    feat: add seed for the setup the database

---
+++ prisma/seed.ts
@@ -0,0 +1,45 @@
+import { PrismaClient } from '@prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
+import pg from 'pg'
+import { config } from 'dotenv'
+
+config()
+
+if (!process.env.DATABASE_URL) {
+    throw new Error('DATABASE_URL is not defined in .env file')
+}
+
+const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL })
+const adapter = new PrismaPg(pool)
+
+const prisma = new PrismaClient({
+    adapter,
+    log: ['query', 'info', 'warn', 'error'],
+})
+
+async function main() {
+    // Create some roles
+    const adminRole = await prisma.role.upsert({
+        where: { name: 'admin' },
+        update: {},
+        create: {
+            name: 'admin',
+        },
+    })
+
+    const userRole = await prisma.role.upsert({
+        where: { name: 'user' },
+        update: {},
+        create: {
+            name: 'user',
+        },
+    })
+
+}
+
+main().catch((e) => {
+    console.error(e);
+    process.exit(1);
+}).finally(async () => {
+    await prisma.$disconnect();
+});
