ef6833a751dc887540fc6257c0d7342c626ba796
Enzo <fournierenzo62@gmail.com>
2026-01-05 11:30:24 +0100

    feat: add elements in prisma

--- prisma/migrations/20260105102820_database/migration.sql
+++ prisma/migrations/20260105102820_database/migration.sql
@@ -35,6 +35,7 @@ CREATE TABLE "lithos" (
     "type" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
+    "elementId" TEXT,
 
     CONSTRAINT "lithos_pkey" PRIMARY KEY ("id")
 );
@@ -59,6 +60,39 @@ CREATE TABLE "collections" (
     CONSTRAINT "collections_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "elements" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "sprite" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "elements_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "weakness_elements" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "elementId" TEXT NOT NULL,
+    "weakAgainstId" TEXT NOT NULL,
+
+    CONSTRAINT "weakness_elements_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "strength_elements" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "elementId" TEXT NOT NULL,
+    "strongAgainstId" TEXT NOT NULL,
+
+    CONSTRAINT "strength_elements_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
 
@@ -77,6 +111,15 @@ CREATE UNIQUE INDEX "roles_name_key" ON "roles"("name");
 -- CreateIndex
 CREATE UNIQUE INDEX "collections_userId_lithosId_key" ON "collections"("userId", "lithosId");
 
+-- CreateIndex
+CREATE UNIQUE INDEX "elements_name_key" ON "elements"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "weakness_elements_elementId_weakAgainstId_key" ON "weakness_elements"("elementId", "weakAgainstId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "strength_elements_elementId_strongAgainstId_key" ON "strength_elements"("elementId", "strongAgainstId");
+
 -- AddForeignKey
 ALTER TABLE "users" ADD CONSTRAINT "users_roleId_fkey" FOREIGN KEY ("roleId") REFERENCES "roles"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
@@ -84,7 +127,22 @@ ALTER TABLE "users" ADD CONSTRAINT "users_roleId_fkey" FOREIGN KEY ("roleId") RE
 ALTER TABLE "authentications" ADD CONSTRAINT "authentications_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
+ALTER TABLE "lithos" ADD CONSTRAINT "lithos_elementId_fkey" FOREIGN KEY ("elementId") REFERENCES "elements"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
 ALTER TABLE "collections" ADD CONSTRAINT "collections_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "collections" ADD CONSTRAINT "collections_lithosId_fkey" FOREIGN KEY ("lithosId") REFERENCES "lithos"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "weakness_elements" ADD CONSTRAINT "weakness_elements_elementId_fkey" FOREIGN KEY ("elementId") REFERENCES "elements"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "weakness_elements" ADD CONSTRAINT "weakness_elements_weakAgainstId_fkey" FOREIGN KEY ("weakAgainstId") REFERENCES "elements"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "strength_elements" ADD CONSTRAINT "strength_elements_elementId_fkey" FOREIGN KEY ("elementId") REFERENCES "elements"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "strength_elements" ADD CONSTRAINT "strength_elements_strongAgainstId_fkey" FOREIGN KEY ("strongAgainstId") REFERENCES "elements"("id") ON DELETE CASCADE ON UPDATE CASCADE;
